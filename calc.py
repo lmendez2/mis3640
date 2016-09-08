16df3ad609b928f5dd14a45969e71a56346e4014
lmendez2 <lmendez2@babson.edu>
2016-09-08 13:21:19 -0400

    calc hw

--- calc.py
+++ calc.py
@@ -1,8 +1,20 @@
-print(46+37+38)
+print (46+37+38)
 print(2016-1996)
 print(15/3)
-print(3*10)
 print(2**6)
+print(60*42+42, 'seconds')
+print(10/1.61, 'miles')
+print(10/1.61/60, 'miles per second')
 
-print(60*42+42, seconds) 
-print(10/1.61, miles) 
+pi=3.1415926 #define pi
+area=(4/3)*pi*5**3 #solve for r=5
+print('%.2f' %(area), 'cubic units')
+print((24.95*.60*60)+(3+.75*59))
+easy= 8.25 #easy pace
+tempo= 7.20 #tempo pace
+total=((8.25+(7.20*3))/60) #total ran #50mins
+print('time 7:42 am')
+grade= ((89-82)/89*100) #compute the percentage increase
+print( '%.1f.' % (grade), '%')
+
+input()
